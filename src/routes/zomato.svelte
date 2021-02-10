1bb835a749ca69cdc5e65777fe33da5a847c52c1
huseyintemel <54399105+huseyintemel@users.noreply.github.com>
2021-02-10 17:49:32 +0300

    Created zomato.svelte in route
    Zomato restoran adres bilgileri
    link:https://bit.ly/3aPSa6t

---
+++ src/routes/zomato.svelte
@@ -0,0 +1,17 @@
+<div class="flex flex-col space-y-2 mx-auto mt-4 rounded-xl max-w-sm border p-4">
+    <p class="text-2xl font-sans text-gray-800">Ara</p>
+    <p class=" text-lg text-red-400 font-sans">+902122570101</p>
+    <p class="text-2xl font-sans text-gray-800">Yol Tarifi</p>
+    <img class="mt-4"  alt="" src="https://maps.zomato.com/php/staticmap?center=41.0803171870,29.0269571971&amp;maptype=zomato&amp;markers=41.0803171870,29.0269571971,pin_res32&amp;sensor=false&amp;scale=2&amp;zoom=16&amp;language=tr&amp;size=240x150&amp;size=400x240&amp;size=650x250">
+    <p class="mt-1 font-sans opacity-75 text-lg ">Akat Mahallesi, Zeytinoğlu Caddesi, İlke Sokak, No: 157A, Beşiktaş, İstanbul</p>
+    <div class="flex flex-row items-center space-x-4 pt-2">
+            <div class="flex flex-row items-center w-28 h-8 border rounded-lg p-2">
+                <svg class="w-5 h-5 fill-current text-gray-500 flex-shrink-0" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 20 20"><title>copy</title><path d="M18.52 6.56c-0.040-0.080-0.1-0.16-0.16-0.22l-5.72-5.72c-0.060-0.060-0.16-0.12-0.26-0.14v0c-0.060-0.040-0.14-0.060-0.24-0.060v0h-4.28c-1.18 0-2.14 0.96-2.14 2.14v0 12.14c0 1.18 0.96 2.14 2.14 2.14v0h8.56c1.2 0 2.16-0.96 2.16-2.14v0-7.86c0 0 0 0 0 0 0-0.1-0.020-0.2-0.060-0.28v0zM12.86 2.86l3.28 3.26h-3.040c-0.14 0-0.24-0.1-0.24-0.22v0zM16.42 15.42h-8.56c-0.4 0-0.72-0.32-0.72-0.72v-12.14c0-0.4 0.32-0.72 0.72-0.72h3.56v4.060c0.020 0.92 0.76 1.66 1.68 1.66h4.040v7.14c0 0.4-0.32 0.72-0.7 0.72h-0.020zM12.86 18.28c0 0.38-0.32 0.7-0.72 0.7h-8.56c-0.4 0-0.72-0.32-0.72-0.7v-12.16c0-0.38 0.32-0.7 0.72-0.7h1.18v-1.44h-1.18c-1.2 0-2.16 0.96-2.16 2.14v0 12.16c0 1.18 0.96 2.14 2.16 2.14v0h8.56c1.18 0 2.14-0.96 2.14-2.14v0-0.48h-1.42z"></path></svg>
+                <p class="p-2 font-sans text-gray-600">Kopyala</p>
+            </div>
+            <div class="flex flex-row items-center w-28 h-8 border rounded-lg p-2">
+                <svg class="w-5 h-5 fill-current text-red-500 flex-shrink-0" xmlns="http://www.w3.org/2000/svg" fill="#ED5A6B" width="20" height="20" viewBox="0 0 20 20"><title>direction</title><path d="M19.2 8.44l-7.22-7.22c-0.52-0.48-1.2-0.8-1.98-0.8s-1.46 0.32-1.98 0.8v0l-7.2 7.22c-0.52 0.5-0.82 1.2-0.82 1.98s0.3 1.46 0.82 1.98l7.2 7.2c0.5 0.52 1.2 0.82 1.98 0.82s1.48-0.3 1.98-0.82l7.22-7.2c0.5-0.52 0.8-1.22 0.8-1.98s-0.3-1.48-0.8-1.98v0zM14.54 9.64l-2.34 2.32c-0.040 0.040-0.080 0.060-0.12 0.060-0.1 0-0.18-0.080-0.18-0.18 0 0 0 0 0 0v0-1.66h-3.56c-0.4 0-0.72 0.32-0.72 0.72v2.38c0 0.26-0.22 0.46-0.48 0.46v0h-0.48c-0.26 0-0.46-0.2-0.46-0.46v0-2.38c0-1.2 0.96-2.16 2.14-2.16v0h3.56v-1.66c0 0 0 0 0 0 0-0.1 0.080-0.18 0.18-0.18 0.040 0 0.080 0.020 0.12 0.060v0l2.34 2.34c0.040 0.040 0.080 0.1 0.080 0.16s-0.040 0.14-0.080 0.18v0z"></path></svg>
+                <p class="p-1 font-sans text-gray-600">Yol Tarifi</p>
+            </div>
+    </div>
+</div>
