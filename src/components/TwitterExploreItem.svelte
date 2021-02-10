fb7568692fce18dbaa8e7ccaa0fd9a033a74d45a
huseyintemel <54399105+huseyintemel@users.noreply.github.com>
2021-02-10 13:06:41 +0300

    Created twitter-explore.svelte and its component
    Twitter gündemkiler kısmı
    link:https://twitter.com/explore

---
+++ src/components/TwitterExploreItem.svelte
@@ -0,0 +1,22 @@
+<script>
+    export let tag;
+    export let tweetCount;
+</script>
+
+<div class="flex flex-col px-3 py-2 ">
+    <div class="flex flex-row justify-between items-center">
+        <p class="text-gray-600 font-sans text-sm">Türkiye konumunda gündemde</p>
+        <div>
+            <svg viewBox="0 0 24 24" class="w-6 h-6 fill-current text-gray-600">
+                <g><circle cx="5" cy="12" r="2"></circle><circle cx="12" cy="12" r="2"></circle><circle cx="19" cy="12" r="2"></circle></g>
+            </svg>
+        </div>
+    </div>
+    <div class="">
+        <p class="font-sans font-bold">{tag}</p>   
+    </div>    
+    <div class="mt-1">
+        <p class="text-gray-600 font-sans text-xs">{tweetCount} Tweet</p>   
+    </div>
+    
+</div>
---
+++ src/routes/twitter-explore.svelte
@@ -0,0 +1,15 @@
+<script>
+import TwitterExploreItem from "../components/TwitterExploreItem.svelte";
+
+</script>
+<div class="flex flex-col mx-auto max-w-2xl divide-y border mt-4">
+    <div class="px-3 py-2">
+        <p class="text-lg font-extrabold font-sans">İlgini çekebilecek gündemler</p>
+    </div>
+   <TwitterExploreItem tag={"#tag1"} tweetCount={"25,4 B"} />
+   <TwitterExploreItem tag={"#tag2"} tweetCount={"47,8 B"} />
+   <TwitterExploreItem tag={"tag3"} tweetCount={"7.403"} />
+   <TwitterExploreItem tag={"#tag4"} tweetCount={"1.028"} />
+   <TwitterExploreItem tag={"#tag5"} tweetCount={"316,8 B"} />
+
+</div>   
