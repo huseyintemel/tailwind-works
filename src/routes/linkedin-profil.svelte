8a324e3820bdf05098989012253b55ace142a8af
huseyintemel <54399105+huseyintemel@users.noreply.github.com>
2021-02-11 17:49:15 +0300

    Created linkedin-profil.svelte
    Linkedin şirket profili
    link:https://www.linkedin.com/company/walmart?trk=companies_directory

---
+++ src/routes/linkedin-profil.svelte
@@ -0,0 +1,35 @@
+<div class="flex flex-col rounded-xl max-w-2xl border mx-auto m-6">
+    <div class="flex flex-row justify-between h-48  max-w-2xl">
+        <img class="rounded-t-xl" src="https://media-exp1.licdn.com/dms/image/C4E1BAQGHkr85nR6xYA/company-background_10000/0/1595016220692?e=2159024400&v=beta&t=XA9bFL4rmWS83a7CsbxwtABGNtf1y73bAs0BRGVulRI"  alt="">
+    </div>
+    <div class="flex flex-row justify-between mx-6 my-4">
+        <img class="w-32 h-32 border p-1 bg-white -mt-24 rounded border-gray-400" alt="" src="https://media-exp1.licdn.com/dms/image/C4E0BAQEcTljsUeQp-A/company-logo_200_200/0/1594303665903?e=2159024400&v=beta&t=VxJuJLlwuSCbrhuL-CwMhcOk89jtGi8g8XBZJBXk1K4" > 
+        <div class="mx-1 cursor-pointer">
+            <svg class="fill-current text-gray-600" xmlns="http://www.w3.org/2000/svg" width="24" height="24" id="ellipsis-horizontal-large" data-supported-dps="24x24">
+                <path d="M2 10h4v4H2v-4zm8 4h4v-4h-4v4zm8-4v4h4v-4h-4z"></path>
+            </svg>
+        </div>
+    </div>
+    <div class="flex flex-row justify-between">
+        <p class="text-2xl font-semibold font-sans mx-6 ">Walmart</p>
+        <div class="flex flex-row px-6">
+            <img class="w-6 h-6 rounded-full" alt="Shannon" src="https://media-exp1.licdn.com/dms/image/C4E03AQG2QsGKXvOHKg/profile-displayphoto-shrink_100_100/0/1516156101113?e=2159024400&amp;v=beta&amp;t=yoiyv8KDzAPCEGD2qZGJd6AoDNzTMn6xEdLmFaQ9l5k">
+            <img class="w-6 h-6 rounded-full" alt="Ted" src="https://media-exp1.licdn.com/dms/image/C4E03AQEFpj1OGN1tyA/profile-displayphoto-shrink_100_100/0/1516157526214?e=2159024400&amp;v=beta&amp;t=78M3m1M22ceGBvtdVv90EW7u0E39O1HMtOYf7lu38xw">
+            <img class="w-6 h-6 rounded-full" alt="Anthony" src="https://media-exp1.licdn.com/dms/image/C4E03AQF4enGSTNbyfQ/profile-displayphoto-shrink_100_100/0/1608853043653?e=2159024400&amp;v=beta&amp;t=wBuf6RWjZklfVk0Hpjsk5bfpwmfNmVgPM82soiaQfGQ">
+            <a class="sm:w-48 w-40 pr-2 font-sans hover:underline text-blue-600 break-words font-semibold" href=https://www.linkedin.com/login/tr>518.491 çalışanın tümünü görüntüle</a>
+        </div>
+    </div>
+    <div class="flex flex-col mx-6">
+        <p class="text-gray-900 font-sans">Perakende</p>
+        <div class="flex flex-row  items-center space-x-2">
+            <p class="font-sans text-gray-700 text-center">Bentonville, Arkansas</p>
+            <p class="text-gray-700 pb-1 text-center text-xs"> . </p>
+            <p class="font-sans text-gray-700 text-center"> 3.001.848 takipçi</p>
+        </div>
+        <p class="font-sans text-gray-600 text-sm">Save Money. Live Better.</p>
+    </div>
+    <div class="flex sm:flex-row flex-col mx-6 mt-4 mb-6">
+        <button class="border w-40 h-10 font-sans font-semibold p-1 rounded-full text-white bg-blue-700 hover:bg-blue-800">İş ilanlarını gör</button>
+        <button class="border w-32 h-10 font-sans font-semibold p-1 sm:ml-3 ml-0 sm:mt-0 mt-3 rounded-full text-blue-700 border-blue-700 hover:bg-gray-200">Takip Edin</button>
+    </div>
+</div>
