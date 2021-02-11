8a1d2676e43a379e9fffc90f34904c7341fa75ec
huseyintemel <54399105+huseyintemel@users.noreply.github.com>
2021-02-11 14:00:10 +0300

    Created facebbok-groups.svelte and its component
    Facebook önerilen sayfalar

---
+++ src/components/FacebookGroupItem.svelte
@@ -0,0 +1,25 @@
+<script>
+    export let src="";
+    export let groupName;
+    export let memberCount;
+    export let dailyCount;
+
+
+</script>
+<div class="flex flex-col  rounded-xl max-w-md border sm:mt-0 mt-4">
+    <div class="flex flex-row justify-between relative max-w-md">
+        <img style="width:424px;height:238px" class="rounded-t-xl " src={src} alt="">
+        <svg class="h-12 w-12 flex-shrink-0 right-0 m-2 text-gray-800 absolute" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+        </svg>
+    </div>
+    <p class="font-sans font-semibold pl-4 pt-1">{groupName}</p>
+    <div class="flex flex-row pl-4 items-center space-x-1">
+        <p class="font-sans text-gray-600 text-center">{memberCount} Üye</p>
+        <p class="text-gray-600 text-center text-sm"> • </p>
+        <p class="font-sans text-gray-600 text-center">Günde {dailyCount} gönderi</p>
+    </div>
+
+    <button class="mx-4 my-4 border max-w-lg font-sans font-semibold p-1 rounded-lg bg-gray-200">Gruba Katıl</button>
+
+</div>
---
+++ src/routes/facebook-groups.svelte
@@ -0,0 +1,17 @@
+<script>
+    import FacebookGroupItem from "../components/FacebookGroupItem.svelte";
+</script>
+
+<div class="flex flex-col justify-center m-10 sm:space-y-4 space-y-0  flex-shrink-0 ">
+    <div class="flex sm:flex-row flex-col  sm:space-x-2  justify-center">
+        <FacebookGroupItem groupName={"Psikoloji"} memberCount={"58 B"} dailyCount={"10"} src={"https://scontent.fada2-2.fna.fbcdn.net/v/t1.0-0/p75x225/145697824_10159241377099656_2542748276527040572_n.jpg?_nc_cat=106&amp;ccb=3&_nc_sid=825194&_nc_ohc=wl0Y7ccGKZMAX-4pzjV&_nc_ht=scontent.fada2-2.fna&tp=6&oh=4dce68528ddd97fafe8ff503d03f1001&oe=604997B6"}  />
+        <FacebookGroupItem groupName={"Matematik Soru Çözüm 2021 TYT- AYT"} memberCount={"61 B"} dailyCount={"180"} src={"https://scontent.fada2-2.fna.fbcdn.net/v/t1.0-0/p75x225/51342244_10156868984993363_6914241337550176256_n.jpg?_nc_cat=106&ccb=3&_nc_sid=825194&_nc_ohc=16IwaG9hw1IAX-DwtSB&_nc_ht=scontent.fada2-2.fna&tp=6&oh=ccdb2641933c9f7574287f7dcc7c0c89&oe=604AFF6E"}  />
+    </div>
+    <div class="flex sm:flex-row flex-col  sm:space-x-2 justify-center">
+        <FacebookGroupItem groupName={"PSİKOLOJi VE İNSAN"} memberCount={"500 B"} dailyCount={"870"} src={"https://scontent.fada2-2.fna.fbcdn.net/v/t1.0-9/s851x315/10612697_1554773551419711_3319596378517428043_n.jpg?_nc_cat=1&ccb=3&_nc_sid=825194&_nc_ohc=fEwZxzaSmysAX9Tllv6&_nc_ht=scontent.fada2-2.fna&tp=7&oh=1d7178538532bdd4f021f8bcae05551d&oe=604A3263"}  />
+        <FacebookGroupItem groupName={"Biyologlar Dayanışma Derneği"} memberCount={"33 B"} dailyCount={"4"} src={"https://scontent.fada2-1.fna.fbcdn.net/v/t31.0-0/p110x80/13495601_1125035014183531_5639822079199051914_o.jpg?_nc_cat=110&ccb=3&_nc_sid=825194&_nc_ohc=NkFZo8TRxRkAX8TfThl&_nc_ht=scontent.fada2-1.fna&tp=6&oh=f1e6cee27d65ce77f5b3d53200d37387&oe=604B443D"}  />
+    </div>
+
+</div>
+
+
