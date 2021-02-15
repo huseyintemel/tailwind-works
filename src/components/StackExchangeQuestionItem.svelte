b1dbd92ecff06b4d2a86e3622bfc1fe0379aa80a
huseyintemel <54399105+huseyintemel@users.noreply.github.com>
2021-02-15 17:48:19 +0300

    Created stackexchange and its component
    Stackexchange anasayfa sorular kısmı
    link:https://stackexchange.com/

---
+++ src/components/StackExchangeQuestionItem.svelte
@@ -0,0 +1,30 @@
+<script>
+    export let questionTitle;
+    export let src;
+    export let tags=[];
+    export let answerCount;
+    export let passedTime;
+    export let questionOwner;
+    export let questionCategory;
+</script>
+<div class="flex flex-col p-2">
+    <div class="flex flex-row space-x-2">
+        <img class="h-8 w-8 cursor-pointer" src={src} alt="">
+        <p class=" font-sans text-lg font-light cursor-pointer hover:text-blue-400 text-blue-600">{questionTitle}</p>
+    </div>
+    <div class="flex flex-row pl-10 space-x-1 items-center whitespace-no-wrap break-words">
+        {#each tags as tag }
+            <button class="text-xs font-sans rounded-full px-2 bg-gray-200  border">{tag}</button>
+        {/each}
+        <p class="text-xs text-gray-500 pl-1"> {answerCount} answers</p>
+        <span class="text-gray-500  text-xs"> | </span>
+        <p class="text-xs text-gray-500">{passedTime} ago</p>
+        <p class="flex flex-row items-center space-x-1">
+            <span class="text-xs text-gray-500">by</span>
+            <span class="text-xs text-blue-600 cursor-pointer">{questionOwner}</span>
+            <span class="text-xs text-gray-500">on</span>
+            <span class="text-xs text-blue-600 cursor-pointer">{questionCategory}</span>
+
+        </p>
+    </div>
+</div>
---
+++ src/routes/stackexchange.svelte
@@ -0,0 +1,24 @@
+<script>
+    import StackExchangeQuestionItem from "../components/StackExchangeQuestionItem.svelte";
+    let tag1=[ ["dnd-5e","spells","sleep"]];
+    let tag2=[["publications","graduate-school"]];
+    let tag3=[["contract-law","law"]];
+</script>
+<div class="flex flex-col border-t border-b divide-y max-w-2xl mx-auto m-6 ">
+    {#each tag1 as tag }
+        <StackExchangeQuestionItem tags={tag} questionTitle={"Can a caster cast a sleep spell on themselves?"} answerCount={"2"} passedTime={"12 hours"} questionOwner={"Alabaster"} questionCategory={"rpg"}  src={"https://cdn.sstatic.net/Sites/rpg/Img/icon-48.png"} />
+    {/each}
+    {#each tag2 as tag }
+        <StackExchangeQuestionItem tags={tag} questionTitle={"Supervisor has said some very disgusting things online, should I pull my name from our paper?"} answerCount={"7"} passedTime={"2 days"} questionOwner={"Charles Averill"} questionCategory={"academia"}  src={"https://cdn.sstatic.net/Sites/physics/Img/icon-48.png?v=f5a02f85db94"} />
+        <StackExchangeQuestionItem tags={tag} questionTitle={"Supervisor has said some very disgusting things online, should I pull my name from our paper?"} answerCount={"7"} passedTime={"2 days"} questionOwner={"Charles Averill"} questionCategory={"academia"}  src={"https://cdn.sstatic.net/Sites/physics/Img/icon-48.png?v=f5a02f85db94"} />
+        <StackExchangeQuestionItem tags={tag} questionTitle={"Supervisor has said some very disgusting things online, should I pull my name from our paper?"} answerCount={"7"} passedTime={"2 days"} questionOwner={"Charles Averill"} questionCategory={"academia"}  src={"https://cdn.sstatic.net/Sites/physics/Img/icon-48.png?v=f5a02f85db94"} />
+    {/each}
+    {#each tag3 as tag }
+        <StackExchangeQuestionItem tags={tag} questionTitle={"Would a contract to pay a trillion dollars in damages be valid?"} answerCount={"3"} passedTime={"20 hours"} questionOwner={"Michael"} questionCategory={"law"}  src={"https://cdn.sstatic.net/Sites/law/Img/icon-48.png"} />
+        <StackExchangeQuestionItem tags={tag} questionTitle={"Would a contract to pay a trillion dollars in damages be valid?"} answerCount={"3"} passedTime={"20 hours"} questionOwner={"Michael"} questionCategory={"law"}  src={"https://cdn.sstatic.net/Sites/law/Img/icon-48.png"} />
+        <StackExchangeQuestionItem tags={tag} questionTitle={"Would a contract to pay a trillion dollars in damages be valid?"} answerCount={"3"} passedTime={"20 hours"} questionOwner={"Michael"} questionCategory={"law"}  src={"https://cdn.sstatic.net/Sites/law/Img/icon-48.png"} />
+        <StackExchangeQuestionItem tags={tag} questionTitle={"Would a contract to pay a trillion dollars in damages be valid?"} answerCount={"3"} passedTime={"20 hours"} questionOwner={"Michael"} questionCategory={"law"}  src={"https://cdn.sstatic.net/Sites/law/Img/icon-48.png"} />
+    {/each}
+   
+   
+</div>
