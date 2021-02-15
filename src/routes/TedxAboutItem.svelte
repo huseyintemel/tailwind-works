6de4d18426100f02e73fa7a9f55af60fc58d59a3
huseyintemel <54399105+huseyintemel@users.noreply.github.com>
2021-02-15 15:04:59 +0300

    Created tedx.svelte and its components and added to Nav menu
    Tedx Programs and Initiatives sayfası
    link:https://www.ted.com/about/programs-initiatives/tedx-program

---
+++ src/routes/TedxAboutItem.svelte
@@ -0,0 +1,10 @@
+<script>
+    export let header;
+    export let headerDetail;
+
+</script>
+<div class="flex flex-col max-w-2xl mt-8">
+    <h4 class="font-thin text-gray-800 text-3xl font-sans">{header}</h4>    
+    <p class="text-sm text-gray-700 mt-2">{headerDetail}</p>
+    
+</div>
